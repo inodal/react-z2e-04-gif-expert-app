23e7b2c9b4d50a44a357a2d77c98dbe84150aa31
Nacho Nodal <i.nodal@kentech-sp.com>
2020-10-17 16:43:55 +0100

    Rename build/ directory as docs/ to be able to deploy in Github Pages

---
+++ docs/precache-manifest.0b0aa7ba9326f6f71c2e068f7d20f725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacc7d1dd23ba5229d800f66d7f34f35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76809f06ea3adc9f51e",
+    "url": "/static/css/main.e5ee5206.chunk.css"
+  },
+  {
+    "revision": "e274a80b3af2078135a2",
+    "url": "/static/js/2.b0f6abe7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b0f6abe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76809f06ea3adc9f51e",
+    "url": "/static/js/main.24abfb58.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
